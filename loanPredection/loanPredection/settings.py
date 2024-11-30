762d7f32b2fd06e083dcef6d71c0a3b5cb5a3483
Dipayan Porel <dipayanporelcllg@gmail.com>
2024-11-30 13:15:58 +0530

    First commit

--- loanPredection/loanPredection/settings.py
+++ loanPredection/loanPredection/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'LoanPrediction',
         'USER': 'postgres',
-        'PASSWORD': 'dipayan84',
+        'PASSWORD': 'abcd',
         'HOST': 'localhost',
         'PORT': '5432',
     }
